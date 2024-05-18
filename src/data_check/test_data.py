6498f58b67d2b836106ee02716088a5691f5651e
Blaise Frison <blaise.frison@gmail.com>
2024-05-18 15:46:37 -0400

    complete data_check

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test the number of rows in the dataframe to ensure it is within reasonable bounds
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test that the prices are within the configured minimum and maximum values
+    """
+    assert data.price.between(min_price, max_price).all()
