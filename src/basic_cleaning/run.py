bbedaa7ed91c98a5f8a72290d8b9aabc51007be7
Blaise Frison <blaise.frison@gmail.com>
2024-05-18 14:13:18 -0400

    complete basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -4,6 +4,9 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 """
 import argparse
 import logging
+import os
+import pandas as pd
+import tempfile
 import wandb
 
 
@@ -13,16 +16,31 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(job_type="basic_cleaning")
-    run.config.update(args)
+    with wandb.init(job_type="basic_cleaning") as run:
+        run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+        logger.info("Downloading data set {args.input_artifact}")
+        artifact_local_path = run.use_artifact(args.input_artifact).file()
+        df = pd.read_csv(
+            artifact_local_path,
+            index_col="id",
+            parse_dates=["last_review"]
+        ).convert_dtypes()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+        logger.info("Cleaning data set")
+        df_cleaned = df[df.price.between(args.min_price, args.max_price)].copy()
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            clean_path = os.path.join(tmp_dir, args.output_artifact)
+            df_cleaned.to_csv(clean_path)
+
+            logger.info("Uploading data set {args.output_artifact} to Wandb")
+            artifact = wandb.Artifact(
+                args.output_artifact,
+                type=args.output_type,
+                description=args.output_description,
+            )
+            artifact.add_file(clean_path)
+            run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
@@ -32,44 +50,44 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=str,
+        help="Name of the data set to download",
+        required=True,
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=str,
+        help="Name of the cleaned data set",
+        required=True,
     )
 
     parser.add_argument(
         "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=str,
+        help="Type of the cleaned data set. This will be used to categorize the artifact in the W&B",
+        required=True,
     )
 
     parser.add_argument(
         "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=str,
+        help="A brief description of the output artifact",
+        required=True,
     )
 
     parser.add_argument(
         "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=float,
+        help="Minimum price below which rows are considered outliers and dropped out",
+        required=True,
     )
 
     parser.add_argument(
         "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        type=float,
+        help="Maximum price above which rows are considered outliers and dropped out",
+        required=True,
     )
 
 
